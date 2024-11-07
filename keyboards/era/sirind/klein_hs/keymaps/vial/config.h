9aa0997b35529acb154eeb6b67bfc1cb65ebbd9f
era <phyojinbin@gmail.com>
2024-11-07 18:21:00 +0900

    Fix incorrect unlock matrix
    Fix incorrect unlock matrix

--- keyboards/era/sirind/klein_hs/keymaps/vial/config.h
+++ keyboards/era/sirind/klein_hs/keymaps/vial/config.h
@@ -3,5 +3,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x83, 0xE4, 0xBA, 0x4F, 0xB8, 0xBA, 0xCE, 0xB2}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 7, 15 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
--- keyboards/era/sirind/klein_sd/keymaps/vial/config.h
+++ keyboards/era/sirind/klein_sd/keymaps/vial/config.h
@@ -3,5 +3,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x0F, 0x63, 0x2A, 0xF5, 0x25, 0xCC, 0x8D, 0x69}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 7, 15 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
